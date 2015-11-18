1aff7c877bc1708faee6ad6c4522fa74d95a4e45
abracadavrex <abracadavrex@gmail.com>
2015-11-18 23:24:23 +0200

    HomeworkAd2 completed; A1 & A3 started.

--- src/week1/Home1.java
+++ src/week1/Home1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Home1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number: ");
+        System.out.print("Enter number: ");
         int number = scanner.nextInt();
         int counter = 0;
 
---
+++ src/week1hw/HomeWorkAd1.java
@@ -0,0 +1,14 @@
+package week1hw;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class HomeWorkAd1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+    }
+}
